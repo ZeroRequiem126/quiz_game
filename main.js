a1a4da248e6a4e7fd383992b40d364d4031a9c15
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-02-05 22:03:47 +0900

    jsファイルにお試し問題を追加

---
+++ main.js
@@ -0,0 +1,15 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？';
+const answers = [
+    'スーパーファミコン',
+    'プレイステーション2',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+document.getElementById('js-question').textContent = question;
+
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
