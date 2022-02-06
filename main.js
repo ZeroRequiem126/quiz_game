9407d52fe4e5b91c998bfe2a66b63ad8f5930baa
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-02-06 19:45:29 +0900

    クイズの問題と選択肢を定義

--- main.js
+++ main.js
@@ -6,10 +6,47 @@ const answers = [
     'ニンテンドーDS'
 ];
 const correct = 'ニンテンドーDS';
+const $button = document.getElementsByTagName('button');
 
-document.getElementById('js-question').textContent = question;
+// クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question;
+    let buttonIndex = 0;
+    let buttonLength = $button.length;
+    while (buttonIndex < buttonLength) {
+        $button[buttonIndex].textContent = answers[buttonIndex];
+        buttonIndex++;
+    }
+}
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+setupQuiz();
+
+// ボタンをクリックしたら正誤判定
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    if (correct === $button[0].textContent) {
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
+document.getElementsByTagName('button')[1].addEventListener('click', () => {
+    if (correct === $button[1].textContent) {
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
+document.getElementsByTagName('button')[2].addEventListener('click', () => {
+    if (correct === $button[2].textContent) {
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
+document.getElementsByTagName('button')[3].addEventListener('click', () => {
+    if (correct === $button[3].textContent) {
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
